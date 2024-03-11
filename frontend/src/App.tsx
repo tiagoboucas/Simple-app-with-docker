a4ab36eff8bed8ceb85095fae21bb2f506d4ff37
tiagoboucas <tiagoboucas@hotmail.com>
2024-03-11 23:49:00 +0000

    Add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css';
 import Header from './components/Header';
+import Navigation from './components/Navigation';
+import MainScreen from './components/MainScreen';
+import SetColor from './components/SetColor';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header text="Theme picker" />
-      
+      <BrowserRouter>
+        <Header text="Theme picker" />
+        <div className='container'>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<MainScreen />} />
+            <Route path="/color-1" element={<SetColor number={1} />} />
+            <Route path="/color-2" element={<SetColor number={2} />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
