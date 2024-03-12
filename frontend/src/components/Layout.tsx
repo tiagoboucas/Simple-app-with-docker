fd2faf0595cddd3cfb320684aa589c7437decad8
tiagoboucas <tiagoboucas@hotmail.com>
2024-03-12 15:49:25 +0000

    add Layout file with background color from blend mode

---
+++ frontend/src/components/Layout.tsx
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router-dom";
+import Navigation from "./Navigation";
+import Header from "./Header";
+import { observer } from 'mobx-react';
+import colorStore from '../stores/ColorStore';
+
+const Layout = observer(() => {
+	return (
+		<>
+			<div
+				className='App'
+				style={{ backgroundColor: colorStore.backgroundColor }}
+			>
+				<Header text='Theme picker' />
+				<div className='container'>
+					<Navigation />
+					<Outlet />
+				</div>
+			</div>
+		</>
+	);
+});
+
+export default Layout;
