99174a680b2ebf9e492cd3674c6cbab90a910ed5
tiagoboucas <tiagoboucas@hotmail.com>
2024-03-13 09:06:57 +0000

    use specific url (cors)

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const colors = JSON.parse(fs.readFileSync('colors.json', 'utf8'));
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     next();
 });
